f6890470dad8ae09f72a1e190d69528d9289e3be
Eli Andino <eliezerorlando19@gmail.com>
2024-07-04 05:40:30 -0400

    Camera code added to html and js files

---
+++ camera-script.js
@@ -0,0 +1,46 @@
+let stream = null;
+
+
+
+document.getElementById('toggleCameraBtn').addEventListener('click', function () {
+    // Getting the video element by id
+const video = document.getElementById('videoElement');
+const button = document.getElementById('toggleCameraBtn');
+
+if (!stream) {
+
+
+
+//checking for browser support
+    if (navigator.mediaDevices.getUserMedia) {
+        //Requesting access to the camera
+        navigator.mediaDevices.getUserMedia({
+            video: true
+        })
+        .then(function (mediaStream) {
+            stream = mediaStream;
+            //Display the camera feed in the video element
+            video.srcObject = stream;
+            video.style.display = 'block';
+            button.textContent = 'Cerrar Camara';
+        })
+        .catch(function (error) {
+            console.error('Error accessing the camera:', error);
+            });
+    } else {
+        console.error('getUserMedia not supported on your browser');
+    } 
+} else {
+    //Stop all tracks inthe stream to deactivate the camera
+    stream.getTracks().forEach(track => track.stop());
+    video.srcObject = null;
+    video.style.display = 'none';
+    stream = null;
+    button.textContent = 'Activate Camera';
+
+        
+    }
+});
+
+
+
