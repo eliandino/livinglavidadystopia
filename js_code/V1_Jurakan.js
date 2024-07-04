ac05392e1fb8ae9da5d5d6954e623297b17c844e
Eli <eliezerorlando19@gmail.com>
2024-07-04 13:48:19 -0400

    I created css and video folders to start adding the videos and make ore structure.

---
+++ js_code/V1_Jurakan.js
@@ -0,0 +1,18 @@
+function onScanSuccess(decodedText, decodedResult) {
+    console.log(`Code matched = ${decodedText}`, decodedResult);
+    document.getElementById('video-source').src = decodedText;
+    document.getElementById('video-container').style.display = 'block';
+    document.getElementById('video-player').load();
+}
+
+function onScanFailure(error) {
+    // Handle scan failure, usually better to ignore and keep scanning.
+    console.warn(`Code scan error = ${error}`);
+}
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+    "qr-reader", 
+    { fps: 10, qrbox: 250 },
+    /* verbose= */ false
+);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
