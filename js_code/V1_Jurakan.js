89f1f75a401a9a8614d9f69c7a3a50ec30df389c
Eli <eliezerorlando19@gmail.com>
2024-07-18 13:41:28 -0400

    update code

--- js_code/V1_Jurakan.js
+++ js_code/V1_Jurakan.js
@@ -1,18 +1,22 @@
-function onScanSuccess(decodedText, decodedResult) {
-    console.log(`Code matched = ${decodedText}`, decodedResult);
-    document.getElementById('video-source').src = decodedText;
-    document.getElementById('video-container').style.display = 'block';
-    document.getElementById('video-player').load();
-}
+// function onScanSuccess(decodedText, decodedResult) {
+//     console.log(`Code matched = ${decodedText}`, decodedResult);
+//     document.getElementById('video-source').src = decodedText;
+//     document.getElementById('video-container').style.display = 'block';
+//     document.getElementById('video-player').load();
+// }
 
-function onScanFailure(error) {
-    // Handle scan failure, usually better to ignore and keep scanning.
-    console.warn(`Code scan error = ${error}`);
-}
+// function onScanFailure(error) {
+//     // Handle scan failure, usually better to ignore and keep scanning.
+//     console.warn(`Code scan error = ${error}`);
+// }
 
-let html5QrcodeScanner = new Html5QrcodeScanner(
-    "qr-reader", 
-    { fps: 10, qrbox: 250 },
-    /* verbose= */ false
-);
-html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+// let html5QrcodeScanner = new Html5QrcodeScanner(
+//     "qr-reader", 
+//     { fps: 10, qrbox: 250 },
+//     /* verbose= */ false
+// );
+// html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+
+document.getElementById('playButton').addEventListener('click', function () {
+    document.getElementById('vid').play();
+});
